8ca870a87dd9060889b1b35fe56d62b098c10232
bautistamiguel966 <miguelbautista182@gmail.com>
2023-02-06 20:14:37 -0300

    Creando las clases en netbeans
    Se crearon las clases e interfaces

---
+++ ProyectoIntegrador_MiguelBautista/BackEnd/src/main/java/com/portfolio/bma/Repository/IPersonaRepository.java
@@ -0,0 +1,11 @@
+
+package com.portfolio.bma.Repository;
+
+import com.portfolio.bma.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona, Long>{
+    
+}

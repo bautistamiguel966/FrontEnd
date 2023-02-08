55072d76817765a9914851eae94c17e81279edfe
bautistamiguel966 <miguelbautista182@gmail.com>
2023-02-08 18:39:46 -0300

    Fin routing
    routing

---
+++ ProyectoIntegrador_MiguelBautista/BackEnd/src/main/java/com/portfolio/bma/Security/Enums/RolNombre.java
@@ -0,0 +1,9 @@
+
+package com.portfolio.bma.Security.Enums;
+
+
+public enum RolNombre {
+    ROLE_ADMIN, ROLE_USER
+    
+    
+}
--- ProyectoIntegrador_MiguelBautista/FrontEnd/src/app/components/logo-ap/logo-ap.component.ts
+++ ProyectoIntegrador_MiguelBautista/FrontEnd/src/app/components/logo-ap/logo-ap.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logo-ap',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class LogoAPComponent {
 
+  constructor(private router: Router){}
+
+  ngOnInit() {}
+
+  login(){
+    this.router.navigate(['/login'])
+  }
 }

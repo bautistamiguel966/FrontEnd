581315d386395f50df3624bf6d5caebae5f29354
bautistamiguel966 <miguelbautista182@gmail.com>
2023-02-19 01:17:37 -0300

    Hard y soft skill
    terminado hard y soft skills

---
+++ ProyectoIntegrador_MiguelBautista/BackEnd/src/main/java/com/portfolio/bma/Repository/Rhys.java
@@ -0,0 +1,12 @@
+
+package com.portfolio.bma.Repository;
+
+import com.portfolio.bma.Entity.hys;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface Rhys extends JpaRepository<hys, Integer>{
+    Optional<hys> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
+}
